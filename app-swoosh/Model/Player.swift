abad98e18d470cc9d7733de0c5cb9d9186947437
Alex Nassif <alexnassif@Alexs-MBP.home>
2017-12-12 16:50:33 -0800

    segues and passing data

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Alex Nassif on 12/12/17.
+//  Copyright © 2017 alexnassif. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkill: String!
+}
